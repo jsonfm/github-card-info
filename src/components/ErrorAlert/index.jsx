07a36aaade8a6fab7e7262f34f141681aeb51741
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-11-05 17:23:43 -0500

    🎨 [UPDATE] ErrorAlert Styles

--- src/components/ErrorAlert/index.jsx
+++ src/components/ErrorAlert/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Snackbar, Alert } from '@mui/material';
+import { Snackbar, Alert, Typography } from '@mui/material';
 import { useAppContext } from "@/store/context";
 import { actionTypes } from "@/reducers";
 
@@ -18,9 +18,11 @@ export function ErrorAlert() {
 
   return (
     <>
-        <Snackbar  open={!!error} autoHideDuration={6000} onClose={handleClose} anchorOrigin={{ vertical:'bottom', horizontal: 'right' }}>
-            <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
-                {error}
+        <Snackbar  open={!!error} autoHideDuration={6000} onClose={handleClose} anchorOrigin={{ vertical:'bottom', horizontal: 'left' }}>
+            <Alert sx={{display: 'flex', alignItems: 'center'}} onClose={handleClose} variant="filled"  severity="error" color="error">
+                <Typography variant="h6">
+                    {error}
+                </Typography>
             </Alert>
         </Snackbar>
     </>
