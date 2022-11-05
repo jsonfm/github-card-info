7435d9a449630d1cd48b965471220ae88842c99b
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-11-05 13:36:03 -0500

    🔧 [UPDATE] vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/github-card-info/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
